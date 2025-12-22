aedae4901ba9836170d940bc39a8d0e110ac0091
Dan Morris <dan@dmonline.uk>
2025-12-22 20:12:18 +0000

    bugfix missing mkdir cmd for lfs-project in chroot

--- 03/run-me-last.sh
+++ 03/run-me-last.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
-cp -r /lfs-project/lfs-12.4-52 $LFS/lfs-project/lfs-12.4-52
+mkdir -v $LFS/lfs-project/
+cp -r /lfs-project/lfs-12.4-52 $LFS/lfs-project/
 
 sudo chown root:root $LFS/lfs-project/*
