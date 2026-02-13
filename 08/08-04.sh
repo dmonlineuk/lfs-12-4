486cce5f7698afadc4bd992eb9fda8ce40abda02
Dan Morris <dan@dmonline.uk>
2026-02-13 16:10:04 +0000

    amended version

--- 08/08-04.sh
+++ 08/08-04.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 here=`pwd`
 cd /sources
-tar xf iana-etc-20251120.tar.gz
-cd iana-etc-20251120
+tar xf iana-etc-20250807.tar.gz
+cd iana-etc-20250807
 
 cp -v services protocols /etc
 
 cd $here
 
-echo "If complete, delete 'rm -rf /sources/iana-etc-20251120'"
+echo "If complete, delete 'rm -rf /sources/iana-etc-20250807'"
