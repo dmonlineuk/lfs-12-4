bd0b4609495396ea1c913c6e3f44c8e8d122042a
Dan Morris <dan@dmonline.uk>
2026-02-13 16:07:51 +0000

    amended version

--- 08/08-03.sh
+++ 08/08-03.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 here=`pwd`
 cd /sources
-tar xf man-pages-6.16.tar.xz
-cd man-pages-6.16
+tar xf man-pages-6.15.tar.xz
+cd man-pages-6.15
 
 rm -v man3/crypt*
 
@@ -10,4 +10,4 @@ make -R GIT=false prefix=/usr install
 
 cd $here
 
-echo "If complete, delete 'rm -rf /sources/man-pages-6.16'"
+echo "If complete, delete 'rm -rf /sources/man-pages-6.15'"
