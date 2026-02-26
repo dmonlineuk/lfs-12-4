de2c565205189336bad472815d89ff420a428da4
Dan Morris <dan@dmonline.uk>
2026-02-26 11:29:21 +0000

    updated echo statement to show what's expected

--- 08/08-21-failed.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-here=`pwd`
-cd /sources/gmp-6.3.0
-
-echo "Note what failures come up and act accordingly. If nothing, then we'll assume all success"
-awk '/# PASS:/{total+=$3} ; END{print total}' gmp-check-log
-
-cd $here
