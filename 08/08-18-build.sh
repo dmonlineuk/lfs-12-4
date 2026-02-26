334e554d33c6650bd678d6b211da760237a56f53
Dan Morris <dan@dmonline.uk>
2026-02-26 09:33:00 +0000

    bugfix: typo on location of configure

--- 08/08-18-build.sh
+++ 08/08-18-build.sh
@@ -7,7 +7,7 @@ cd dejagnu-1.6.3
 mkdir -v build && cd build
 
 time {
-    ./configure --prefix=/usr
+    ../configure --prefix=/usr
 
     makeinfo --html --no-split -o doc/dejagnu.html ../doc/dejagnu.texi
     makeinfo --plaintext       -o doc/dejagnu.txt  ../doc/dejagnu.texi
