0d6f4733e6b5b0c0aa490f92720debfe9a910f3e
Dan Morris <dan@dmonline.uk>
2026-02-26 11:24:22 +0000

    update to include an echo for what we expect

--- 08/08-20-failed.sh
+++ 08/08-20-failed.sh
@@ -2,6 +2,7 @@
 here=`pwd`
 cd /sources/binutils-2.45/build
 
+echo "Note what failures come up and act accordingly. If nothing, then we'll assume all success"
 grep '^FAIL:' $(find -name '*.log')
 
 cd $here
