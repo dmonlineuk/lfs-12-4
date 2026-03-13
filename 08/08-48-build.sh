ae7ef7beb3b33e231405efba75ad2b7be485cf0f
Dan Morris <dan@dmonline.uk>
2026-03-13 11:31:24 +0000

    bugfix: typo on .config as it is not .configure as it usually is

--- 08/08-48-build.sh
+++ 08/08-48-build.sh
@@ -4,7 +4,7 @@ cd /sources
 tar xf openssl-3.5.2.tar.gz
 cd openssl-3.5.2
 
-./configure --prefix=/usr         \
+./config --prefix=/usr         \
          --openssldir=/etc/ssl \
          --libdir=lib          \
          shared                \
